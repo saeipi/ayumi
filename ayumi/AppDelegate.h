784b0880fef89b7103f91f653d41566f8c2fe2a9
saeipi <saeipi@ayumi.local>
2017-07-18 12:50:57 +0800

    Initial Commit

---
+++ ayumi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ayumi
+//
+//  Created by saeipi on 2017/7/18.
+//  Copyright © 2017年 saeipi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ayumi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ayumi
+//
+//  Created by saeipi on 2017/7/18.
+//  Copyright © 2017年 saeipi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
